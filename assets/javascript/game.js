f44a466a12d867574fe09d69b9bcfdc70d8b8122
Kelly davis <kellyadavis7@gmail.com>
2018-12-28 17:50:05 -0500

    should i use jQuery?? should i do the psychic game? wtf

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    // Initial Variables
+    var words = ["dog", "puppy", "labrador", "pug"];
+    // messages to be displayed
+    var messages = ["Aw sorry u lose", "u win", "play again?"];
+    // initial lives
+    var lives = 8;
+    // decrease 'lives' by 1 for each wrong 'guessedWord' (try accessing them via '.tryLetters') until it reaches 0
+    for (let x = 8; x < 8; x--);
+        
+    // computer selects random word from ['words'] and stores it in 'computerPick'
+   var computerPick = words[Math.floor(Math.random() * words.length)];
+    console.log(computerPick);
+
+
+    // Here we create the on click event that gets the user's pick
+    $(".btn-choice").on("click", function() {
+    
+        // store user's letter guess in '#guessWord' into var guessedWord
+        var guessedWord = $("#guessWord");
+        // if 'guessedWord' is in 'computerPick':
+        var y = computerPick.includes(guessedWord);
+        // display 'guessedWord' in '#hangmanContainer'
+        if (y === true);
+            $("#hangmanContainer").append(guessedWord);
+        // if 'guessedWord' is not in 'computerPick', display 'guessedWord' in '.tryLetters (.guessingWordContainer?)'
+        if (y === false);
+            $(".tryLetters").append(guessedWord);
+        
+
+        
+    // when 'lives' == 0: display 'messages[0, 2]' in <p>, create new buttons y/n 
+    // if 'lives' >= 0, display 'messages[1, 2]' in <p>, create new buttons y/n 
+
+
+
+
+
+
+
+    });
+  });
