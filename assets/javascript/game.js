52557f8c41f89e07d787a893f0164145bbf62306
Kelly davis <kellyadavis7@gmail.com>
2018-12-29 17:40:33 -0500

    works better, functional lives counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,10 @@ var computerPick = words[Math.floor(Math.random() * words.length)];
 var rightWord = [];
 var wrongWord = [];
 var underScore = [];
-// lives
-var lives;
-// count correct guesses
-var counter;
+var wins = 0;
+var loss = 0;
+var guessesLeft = 9;
+var winCounter;
 
 // dom manipulation/get elements
 console.log(computerPick);
@@ -23,46 +23,69 @@ var docWG = document.getElementsByClassName("wrongGuess");
 // main
 // ============================
 
-// create underscores based on length of word
-var generateUnderscore = () => {
-    for(var i = 0; i < computerPick.length; i++) {
-        underScore.push("_");
+function startGame() {
+    // create underscores based on length of word
+    var generateUnderscore = () => {
+        for(var i = 0; i < computerPick.length; i++) {
+            underScore.push("_");
+        }
+        return underScore;
     }
-    return underScore;
-}
 
-console.log(generateUnderscore())
+    console.log(generateUnderscore())
 
-// get user's guess
-document.addEventListener('keypress', (event) => {
-    var keyword = String.fromCharCode(event.keyCode);
-    // if user's guess is right
-    if(computerPick.indexOf(keyword) > -1) {
-        // push the right words to 'rightWord' array
-        rightWord.push(keyword);
-        // replace underscore with right letter
-        underScore[computerPick.indexOf(keyword)] = keyword;
-        docUS[0].innerHTML = underScore.join(" ");
-        docRG[0].innerHTML = rightWord; 
+    // reset
+    guessesLeft = 10;
 
-        // checks to see if user word matches guesses
+    // html
+    document.getElementById("guesses-left").textContent = guessesLeft;
+
+    function winLose() {
         if(underScore.join("") == computerPick) {
             console.log("you win!");
+            }
+        else if(guessesLeft === 0) {
+            alert("loser");
         }
     }
 
-    // if user's guess is wrong
-    else {
-        wrongWord.push(keyword);
-        // push wrong words to [wrongWord]
-        docWG[0].innerHTML = wrongWord;
-    
-        // //  decreases lives starting at 8 by 1 for every new wrongWord
-        //     document.getElementsByClassName("livesScore").innerHTML = (livesCounter);
-        //     console.log(livesCounter);
-        
-    }
-    // Show lives
+    // get user's guess
+    document.addEventListener('keypress', (event) => {
+        var keyword = String.fromCharCode(event.keyCode);
+        // if user's guess is right
+        if(computerPick.indexOf(keyword) > -1) {
+            for(var i = 0; i < computerPick.length; i++) {
+                if(computerPick[i] === keyword) {
+                    // push the right words to 'rightWord' array
+                    rightWord.push(keyword);
+                    underScore[i] = keyword;
+                    // underScore[computerPick.indexOf(keyword)] = keyword;
+                    docUS[0].innerHTML = underScore.join(" ");
+                    docRG[0].innerHTML = rightWord; 
+                    // replace underscore with right letter
+                    winCounter++;
+                    winLose();
+                }
+            }
+            
+        }
 
+        // if user's guess is wrong
+        else {
+            guessesLeft--;
+            wrongWord.push(keyword);
+            // //  decreases lives starting at 8 by 1 for every new wrongWord
+            // push wrong words to [wrongWord]
+            docWG[0].innerHTML = wrongWord;
 
-});
+            //     document.getElementsByClassName("livesScore").innerHTML = (livesCounter);
+            document.getElementById("guesses-left").textContent = guessesLeft;
+            
+        }
+        // Show lives
+
+    })
+};
+// main
+// ========
+startGame();
